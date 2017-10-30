6b5aa671d7463bae9af846ba242cd1d86f954bdd
Philippe Luchansky <philuchansky@gmail.com>
2017-10-29 17:24:21 -0700

    completed restful api routes for users

--- controllers/users.js
+++ controllers/users.js
@@ -11,5 +11,26 @@ module.exports = {
 		User.findById(req.params.id, (err, user) => {
 			res.json(user)
 		})
+	},
+
+	create: (req, res) => {
+		User.create(req.body, (err, user) => {
+			res.json({success: true, message: "User created.", user})
+		})
+	},
+
+	update: (req, res) => {
+		User.findById(req.params.id, (err, user) => {
+			Object.assign(user, req.body)
+			user.save((err, updatedUser) => {
+				res.json({success: true, message: "User updated.", user})
+			})
+		})
+	},
+
+	destroy: (req, res) => {
+		User.findByIdAndRemove(req.params.id, (err, user) => {
+			res.json({success: true, message: "User deleted.", user})
+		})
 	}
 }
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,11 @@ const
 
 usersRouter.route('/')
 	.get(usersCtrl.index)
+	.post(usersCtrl.create)
+
+usersRouter.route('/:id')
+	.get(usersCtrl.show)
+	.patch(usersCtrl.update)
+	.delete(usersCtrl.destroy)
 
 module.exports = usersRouter
