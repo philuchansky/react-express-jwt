78f9fa19659a5fe96e4bcbb2e7697e77c79fb609
Philippe Luchansky <philuchansky@gmail.com>
2017-10-30 15:30:27 -0700

    strip out unneeded packages, require milligram and styles.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'milligram'
+import './styles.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
