42ea3893151edac73d83ba43227842c6f4079d8a
Philippe Luchansky <philuchansky@gmail.com>
2017-10-30 15:33:02 -0700

    wrap <App /> in <BrowserRouter />

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
 import 'milligram'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
