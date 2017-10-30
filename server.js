6ebbd8ab90bd41595ee4ce191ef1c919732a1f0f
Philippe Luchansky <philuchansky@gmail.com>
2017-10-29 17:02:12 -0700

    setup express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const
+	express = require('express'),
+	app = express(),
+	logger = require('morgan'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/react-express-jwt',
+	PORT = process.env.PORT || 3001
+
+app.get('/api', (req, res) => {
+	res.json({message: "API root."})
+})
+
+app.listen(PORT, (err) => {
+	console.log(err || `Server running on port ${PORT}.`)
+})
