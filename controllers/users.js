5dc7db7384fbbe60528da4aa67b9c18a262a43a1
Philippe Luchansky <philuchansky@gmail.com>
2017-10-29 17:44:42 -0700

    add documentation

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,28 @@
 const User = require('../models/User.js')
 
 module.exports = {
+	// list all users
 	index: (req, res) => {
 		User.find({}, (err, users) => {
 			res.json(users)
 		})
 	},
 
+	// get one user
 	show: (req, res) => {
 		User.findById(req.params.id, (err, user) => {
 			res.json(user)
 		})
 	},
 
+	// create a new user
 	create: (req, res) => {
 		User.create(req.body, (err, user) => {
 			res.json({success: true, message: "User created.", user})
 		})
 	},
 
+	// update an existing user
 	update: (req, res) => {
 		User.findById(req.params.id, (err, user) => {
 			Object.assign(user, req.body)
@@ -28,6 +32,7 @@ module.exports = {
 		})
 	},
 
+	// delete an existing user
 	destroy: (req, res) => {
 		User.findByIdAndRemove(req.params.id, (err, user) => {
 			res.json({success: true, message: "User deleted.", user})
--- models/User.js
+++ models/User.js
@@ -7,14 +7,18 @@ const
 		password: { type: String, required: true }
 	})
 
+// adds a method to a user document object to create a hashed password
 userSchema.methods.generateHash = function(password) {
 	return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
 }
 
+// adds a method to a user document object to check if provided password is correct
 userSchema.methods.validPassword = function(password) {
 	return bcrypt.compareSync(password, this.password)
 }
 
+// middleware: before saving, check if password was changed,
+// and if so, encrypt new password before saving:
 userSchema.pre('save', function(next) {
 	if(this.isModified('password')) {
 		this.password = this.generateHash(this.password)
