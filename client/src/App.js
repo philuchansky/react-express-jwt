032656a973c0b3b4a68382e7bd22da4b81a814e6
Philippe Luchansky <philuchansky@gmail.com>
2017-10-30 17:32:26 -0700

    VIP link

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       <div className="App">
         <div>
           <Link to="/">Home</Link>
+          <Link to="/vip">VIP</Link>
           <Link to="/login">Log In</Link>
           <Link to="/signup">Sign Up</Link>
         </div>
