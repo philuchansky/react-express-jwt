49bffefc4846a218cc80580fc16f4d955d2c665d
Philippe Luchansky <philuchansky@gmail.com>
2017-10-30 22:29:23 -0700

    validate uniqueness of user email

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const
 	bcrypt = require('bcrypt-nodejs'),
 	userSchema = new mongoose.Schema({
 		name: { type: String },
-		email: { type: String, required: true },
+		email: { type: String, required: true, unique: true },
 		password: { type: String, required: true }
 	})
 
